377aa100979b58c7e151c08573c4c276d88b4e87
Michelle~ <theriomich@gmail.com>
2024-05-27 15:30:30 -0400

    Solicitud: Cree solicitud HTTP simple para enviar texto a la ruta inicio y ruta contacto

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ let express= require("express")
 
 let app= express()
 
+
+app.get("/", function(peticion, respuesta){
+respuesta.send("Ruta INICIO")//SEND permite enviar al navegador un texto
+});
+
+app.get("/contacto", function(peticion, respuesta){
+  respuesta.send("Ruta CONTACTO")
+  });
+
+//METODOS COMO GET, POST, PUT, SON METODOS DE SOLICITUDES HTTP
+
 app.listen(3000, function(peticion,respuesta){
   console.log("Hola Mundo")
 });
+
